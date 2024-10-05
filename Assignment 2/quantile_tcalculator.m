38cc7feb9908dabe490eb914eeafd9d732ba9c0f
Andreu <andreubotor01@gmail.com>
2024-10-05 14:16:16 +0200

    fix2

--- Assignment 2/quantile_tcalculator.m
+++
@@ -1,8 +0,0 @@
-i = 1;
-studenttdistribution_quantiles = [];
-while i <= 30
-    studenttdistribution_quantiles = [studenttdistribution_quantiles, tinv(0.975, i-1)];
-    i = i+1;
-end
-
-display(studenttdistribution_quantiles)
